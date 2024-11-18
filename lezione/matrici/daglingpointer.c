ff5bd356f970771c722ae7f3e555eba8f67ccdf8
runzac <91915526+alerunza@users.noreply.github.com>
2024-11-18 15:08:20 +0100

    malloc esempio

--- lezione/matrici/daglingpointer.c
+++ lezione/matrici/daglingpointer.c
@@ -6,6 +6,11 @@ int* copy(int a[], int size);
 int main(void) {
     int nums[4]={3,4,5,6};
     
+    for(int i=0;i<4;i++){
+        printf("%d ",nums[i]);
+    }
+    printf("\n");
+
     int* nums2 = copy(nums, 4);//dangling pointer HERE
     
     for(int i=0;i<4;i++){
@@ -17,11 +22,14 @@ int main(void) {
 int* copy(int a[], int size){
     int i;
 
-    int a2[size];
+    // non va bene e di conseguenza si usa il malloc 
+    //int a2[size];
     
-    for(int i=0;i<size;i++){
+    //heap array
+    int* a2 = (int*) malloc(size * sizeof(int));
+    
+    for(int i = 0; i < size; i++){
         a2[i] = a[i];
-    
     }
 
     return a2;
