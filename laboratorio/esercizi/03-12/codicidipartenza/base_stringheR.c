63c6d065b078fd2a0baa53d38e66d76c2e6dc80b
runzac <91915526+alerunza@users.noreply.github.com>
2024-12-03 11:08:07 +0100

    base_str completed

--- laboratorio/esercizi/03-12/codicidipartenza/base_stringheR.c
+++ laboratorio/esercizi/03-12/codicidipartenza/base_stringheR.c
@@ -46,8 +46,43 @@ int main(void)
     printf("%d [Atteso: 1]\n", str_ugualiR("", ""));
 }
 
-//------------------------------------------------------------------
+// funzione ricorsiva che stampa la stringa pStr, 
+// un carattere alla volta.
+void stampa_strR(const char* pStr){
+    if(*pStr == '\0'){
+        return;
+    } else{
+        printf("%c", *pStr);
+        stampa_strR(pStr + 1);
+    }
+}
 
-//COMPLETARE
+// funzione ricorsiva che stampa la stringa pStr, 
+// un carattere alla volta, in ordine "rovesciato".
+// Es. con pStr="ciao" viene stampato "oaic".
+void stampa_rev_strR(const char* pStr){
+    if(*pStr == '\0'){
+        return;
+    } else{
+        stampa_rev_strR(pStr + 1);
+        printf("%c", *pStr);
+    }
+}
 
-//------------------------------------------------------------------
+// funziona ricorsiva che confronta due stringhe, un carattere alla volta,
+// e ritorna true se le due stringhe sono uguali e della stessa lunghezza
+bool str_ugualiR(const char* pStrA, const char* pStrB){
+    if(*pStrA == '\0' || *pStrB == '\0'){
+        if(*pStrA == '\0' && *pStrB == '\0'){
+            return true;
+        } else{
+            return false;
+        }
+    } else{
+        if (*pStrA == *pStrB){
+            return true && str_ugualiR(pStrA + 1, pStrB + 1);
+        } else{
+            return false;
+        }
+    }
+}
